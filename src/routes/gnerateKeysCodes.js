f8b810e229cac6d5b70bc5d77a3bd588c9cf8f5d
ibrahim <ahmeeedji01@gmail.com>
2024-09-14 09:23:21 +0200

    9

--- src/routes/gnerateKeysCodes.js
+++ src/routes/gnerateKeysCodes.js
@@ -112,7 +112,7 @@ function delay(ms) {
 
 async function createCodes() {
   return new Promise(async (res,rej) => {
-    let j = 9;
+    let j = 0;
     const promosCodes = []; // مصفوفة لتخزين الأكواد التي يتم توليدها
     for(let s = 0 ; s <= 5 ; s++)
     {
